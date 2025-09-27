867a990c54b12644086484a6d1f28e6f22fca351
rdoo <rafdomaradzki@gmail.com>
2025-09-27 20:30:53 +0200

    style: add .editorconfig and reformat code

--- README.md
+++ README.md
@@ -1,40 +1,47 @@
 # Run Node Webpack Plugin
+
 Webpack plugin that starts a node.js script after webpack compilation and smartly restarts it after subsequent recompilations
 
 ## Installation
+
 ```sh
 $ npm install run-node-webpack-plugin --save-dev
 ```
 
 ## Usage
+
 **webpack.config.js**
+
 ```js
 const RunNodeWebpackPlugin = require('run-node-webpack-plugin');
 
 module.exports = {
-    // ...
-    plugins: [
-        new RunNodeWebpackPlugin()
-    ],
-    // ...
+  // ...
+  plugins: [new RunNodeWebpackPlugin()],
+  // ...
 };
 ```
+
 Plugin's primary use case is when you are developing a node.js app with webpack and want that app to restart whenever webpack finishes compiling its bundle.
 
 ## Advantages
+
 - More efficient in webpack development scenarios than `nodemon`, `pm2`, `forever`, etc.
 - Contrary to other similar plugins can restart script only when an actual change in webpack bundle occurs
 - Rich customization possible via plugin options
 
 ## Options
+
 All the options are optional and are configured so that in most cases you should not want to change them.
 
 ### `scriptToRun: string`
+
 Node script which will be run after webpack compilations. You can provide a file name from webpack output or path to an external file.
 
 **Default:** By default when webpack config contains only one output file then that file will be run. If more files are present then file named `server.js` or `index.js` (whichever exists) will be run
 
 **Examples**
+
 ```js
 // ...
 entry: {
@@ -51,7 +58,9 @@ plugins: [
 ],
 // ...
 ```
+
 In this example by default `server.js` would be run but you can change it to `otherScript.js`.
+
 ```js
 // ...
 entry: 'src/index.js',
@@ -65,6 +74,7 @@ plugins: [
 ],
 // ...
 ```
+
 In this example by default `webpackOutputScript.js` would be run but you can change it to an external script not connected with webpack build process, for example: `./path/to/external/file.js`.
 
 ```js
@@ -81,25 +91,29 @@ plugins: [
     new RunNodeWebpackPlugin({
         scriptToRun: 'server.js',
         processArgs: {
-            execArgv:['--inspect-brk']
-        } 
+            execArgv: ['--inspect-brk']
+        }
     })
 ],
 // ...
 ```
+
 In this example a debugger is attached when running the script.
 
 ### `runOnlyOnChanges: boolean`
+
 When set to `true` plugin will run the script only when an actual change in webpack output files happens, otherwise after every webpack compilation.
 
 **Default: `true`**
 
 ### `scriptsToWatch: string[]`
+
 Array of webpack ouput file names that plugin should watch for changes. Works only if `runOnlyOnChanges` is set to `true`.
 
 **Default:** By default plugin watches all webpack output files
 
 **Examples**
+
 ```js
 // ...
 entry: {
@@ -116,34 +130,41 @@ plugins: [
 ],
 // ...
 ```
+
 In this example by default plugin would run `server.js` whenever a change to `server.js` or `script.js` happens but you can exclude `script.js` from change detection by providing only `server.js` to `scriptsToWatch` option.
 
 ### `runOnlyInWatchMode: boolean`
+
 When set to `true` plugin will run the script only when webpack is in a watch mode.
 
 **Default: `false`**
 
 ### `runOnlyInNormalMode: boolean`
+
 When set to `true` plugin will run the script only when webpack is NOT in a watch mode.
 
 **Default: `false`**
 
 ### `ignoreErrors: boolean`
+
 When set to `true` plugin will try to run the script even when webpack compilation throws errors.
 
 **Default: `false`**
 
 ### `nodeArgs: string[]`
+
 List of string arguments to pass to the node script.
 [See more information](https://nodejs.org/api/child_process.html#child_processforkmodulepath-args-options)
 
 **Default: `[]`**
 
 ### `processArgs: Object`
+
 List of options to pass to the process executing the node script.
 [See more information](https://nodejs.org/api/child_process.html#child_processforkmodulepath-args-options)
 
 **Default: `[]`**
 
 ## License
+
 Copyright © 2018 [rdoo](https://github.com/rdoo). Released under the MIT license.
--- src/index.ts
+++ src/index.ts
@@ -6,187 +6,225 @@ import { Compiler, Stats } from 'webpack';
 import { Logger, LoggerMessages } from './logger';
 
 export interface RunNodeWebpackPluginOptions {
-    scriptToRun?: string;
-    scriptsToWatch?: string[];
-    runOnlyOnChanges?: boolean;
-    runOnlyInWatchMode?: boolean;
-    runOnlyInNormalMode?: boolean;
-    ignoreErrors?: boolean;
-    nodeArgs?: string[];
-    processArgs?: ForkOptions;
+  scriptToRun?: string;
+  scriptsToWatch?: string[];
+  runOnlyOnChanges?: boolean;
+  runOnlyInWatchMode?: boolean;
+  runOnlyInNormalMode?: boolean;
+  ignoreErrors?: boolean;
+  nodeArgs?: string[];
+  processArgs?: ForkOptions;
 }
 
 export default class RunNodeWebpackPlugin {
-    options: RunNodeWebpackPluginOptions;
-    isWebpackInWatchMode: boolean = false;
-    isFirstRun: boolean = true;
-    errorsInPrevCompilation: boolean = false;
-    scriptName: string;
-    scriptPath: string;
-    scriptProcess: ChildProcess;
+  options: RunNodeWebpackPluginOptions;
+  isWebpackInWatchMode: boolean = false;
+  isFirstRun: boolean = true;
+  errorsInPrevCompilation: boolean = false;
+  scriptName: string;
+  scriptPath: string;
+  scriptProcess: ChildProcess;
 
-    constructor(passedOptions?: RunNodeWebpackPluginOptions) {
-        const defaultOptions: RunNodeWebpackPluginOptions = {
-            scriptToRun: null,
-            scriptsToWatch: null,
-            runOnlyOnChanges: true,
-            runOnlyInWatchMode: false,
-            runOnlyInNormalMode: false,
-            ignoreErrors: false,
-            nodeArgs: [],
-            processArgs: {},
-        };
-        this.options = Object.assign(defaultOptions, passedOptions);
-    }
+  constructor(passedOptions?: RunNodeWebpackPluginOptions) {
+    const defaultOptions: RunNodeWebpackPluginOptions = {
+      scriptToRun: null,
+      scriptsToWatch: null,
+      runOnlyOnChanges: true,
+      runOnlyInWatchMode: false,
+      runOnlyInNormalMode: false,
+      ignoreErrors: false,
+      nodeArgs: [],
+      processArgs: {},
+    };
+    this.options = Object.assign(defaultOptions, passedOptions);
+  }
 
-    apply(compiler: Compiler) {
-        compiler.hooks.watchRun.tap('RunNodeWebpackPlugin', () => (this.isWebpackInWatchMode = true));
+  apply(compiler: Compiler) {
+    compiler.hooks.watchRun.tap(
+      'RunNodeWebpackPlugin',
+      () => (this.isWebpackInWatchMode = true)
+    );
 
-        compiler.hooks.done.tap('RunNodeWebpackPlugin', (stats: Stats) => {
-            // dont run if webpack is not in a watch mode and runOnlyInWatchMode option is truthy
-            if (!this.isWebpackInWatchMode && this.options.runOnlyInWatchMode) {
-                return;
+    compiler.hooks.done.tap('RunNodeWebpackPlugin', (stats: Stats) => {
+      // dont run if webpack is not in a watch mode and runOnlyInWatchMode option is truthy
+      if (!this.isWebpackInWatchMode && this.options.runOnlyInWatchMode) {
+        return;
+      }
+
+      // dont run if webpack is in a watch mode and runOnlyInNormalMode option is truthy
+      if (this.isWebpackInWatchMode && this.options.runOnlyInNormalMode) {
+        return;
+      }
+
+      // dont run if webpack compilation contains errors and ignoreErrors option is falsy
+      if (stats.hasErrors() && !this.options.ignoreErrors) {
+        this.errorsInPrevCompilation = true;
+        return;
+      }
+
+      const { compilation } = stats;
+      const { compiler, emittedAssets, assets: outputAssets } = compilation;
+      const outputAssetNames: string[] = Object.keys(outputAssets);
+      const outputPath = compilation.getPath(compiler.outputPath);
+
+      // check if output assets dont exist. idk if this can really happen
+      if (outputAssetNames.length < 1) {
+        Logger.error(LoggerMessages.NO_OUTPUT_ASSETS);
+        return;
+      }
+
+      let shouldRun: boolean = false;
+
+      if (!this.options.runOnlyOnChanges || this.errorsInPrevCompilation) {
+        // always run node script if runOnlyOnChanges option is falsy or there were errors in previous compilation
+        shouldRun = true;
+        this.errorsInPrevCompilation = false;
+      } else {
+        // else check watched scripts for changes
+        if (
+          this.options.scriptsToWatch &&
+          this.options.scriptsToWatch.length > 0
+        ) {
+          // if scriptsToWatch option is set then check if any of the given scripts changed
+          for (const scriptName of this.options.scriptsToWatch) {
+            const matchedName: string = findMatchingScriptName(
+              scriptName,
+              outputAssetNames
+            );
+            if (
+              matchedName &&
+              ((emittedAssets && emittedAssets.has(matchedName)) ||
+                (outputAssets[matchedName] as any).emitted)
+            ) {
+              shouldRun = true;
+              break;
             }
-
-            // dont run if webpack is in a watch mode and runOnlyInNormalMode option is truthy
-            if (this.isWebpackInWatchMode && this.options.runOnlyInNormalMode) {
-                return;
+          }
+        } else {
+          // if scriptsToWatch option is NOT set then check if any of the output assets changed
+          for (const assetName of outputAssetNames) {
+            if (
+              (emittedAssets && emittedAssets.has(assetName)) ||
+              (outputAssets[assetName] as any).emitted
+            ) {
+              shouldRun = true;
+              break;
             }
+          }
+        }
+      }
 
-            // dont run if webpack compilation contains errors and ignoreErrors option is falsy
-            if (stats.hasErrors() && !this.options.ignoreErrors) {
-                this.errorsInPrevCompilation = true;
-                return;
+      if (this.isFirstRun) {
+        shouldRun = true;
+        this.isFirstRun = false;
+      }
+
+      if (!shouldRun) {
+        return;
+      }
+
+      if (!this.scriptPath) {
+        if (this.options.scriptToRun) {
+          // if scriptToRun option is set then check if it is in webpack output and get its exact path
+          // if it can not be found in webpack output then check if it exists in the file system
+          this.scriptName = findMatchingScriptName(
+            this.options.scriptToRun,
+            outputAssetNames
+          );
+          if (this.scriptName) {
+            this.scriptPath = join(outputPath, this.scriptName);
+          } else if (existsSync(this.options.scriptToRun)) {
+            this.scriptName = parse(this.options.scriptToRun).base;
+            this.scriptPath = normalize(this.options.scriptToRun);
+          }
+
+          if (!this.scriptPath) {
+            Logger.error(
+              LoggerMessages.NO_SCRIPT_NAME1 +
+                this.options.scriptToRun +
+                LoggerMessages.NO_SCRIPT_NAME2 +
+                outputAssetNames +
+                LoggerMessages.NO_SCRIPT_NAME3
+            );
+            return;
+          }
+        } else {
+          // if scriptToRun option is NOT set then try to guess which script should be run
+          if (outputAssetNames.length === 1) {
+            // if theres only 1 file in output assets choose it
+            this.scriptName = outputAssetNames[0];
+            this.scriptPath = join(outputPath, this.scriptName);
+          } else {
+            // otherwise try to find scripts named 'server.js' or 'index.js'
+            this.scriptName =
+              findMatchingScriptName('server.js', outputAssetNames) ||
+              findMatchingScriptName('index.js', outputAssetNames);
+            if (this.scriptName) {
+              this.scriptPath = join(outputPath, this.scriptName);
             }
+          }
+        }
+      }
 
-            const { compilation } = stats;
-            const { compiler, emittedAssets, assets: outputAssets } = compilation;
-            const outputAssetNames: string[] = Object.keys(outputAssets);
-            const outputPath = compilation.getPath(compiler.outputPath);
+      if (!this.scriptPath) {
+        Logger.error(
+          LoggerMessages.NO_SCRIPT_PATH1 +
+            outputAssetNames +
+            LoggerMessages.NO_SCRIPT_PATH2
+        );
+        return;
+      }
 
-            // check if output assets dont exist. idk if this can really happen
-            if (outputAssetNames.length < 1) {
-                Logger.error(LoggerMessages.NO_OUTPUT_ASSETS);
-                return;
-            }
-
-            let shouldRun: boolean = false;
-
-            if (!this.options.runOnlyOnChanges || this.errorsInPrevCompilation) {
-                // always run node script if runOnlyOnChanges option is falsy or there were errors in previous compilation
-                shouldRun = true;
-                this.errorsInPrevCompilation = false;
-            } else {
-                // else check watched scripts for changes
-                if (this.options.scriptsToWatch && this.options.scriptsToWatch.length > 0) {
-                    // if scriptsToWatch option is set then check if any of the given scripts changed
-                    for (const scriptName of this.options.scriptsToWatch) {
-                        const matchedName: string = findMatchingScriptName(scriptName, outputAssetNames);
-                        if (matchedName && (emittedAssets && emittedAssets.has(matchedName) || (outputAssets[matchedName] as any).emitted)) {
-                            shouldRun = true;
-                            break;
-                        }
-                    }
-                } else {
-                    // if scriptsToWatch option is NOT set then check if any of the output assets changed
-                    for (const assetName of outputAssetNames) {
-                        if (emittedAssets && emittedAssets.has(assetName) || (outputAssets[assetName] as any).emitted) {
-                            shouldRun = true;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            if (this.isFirstRun) {
-                shouldRun = true;
-                this.isFirstRun = false;
-            }
-
-            if (!shouldRun) {
-                return;
-            }
-
-            if (!this.scriptPath) {
-                if (this.options.scriptToRun) {
-                    // if scriptToRun option is set then check if it is in webpack output and get its exact path
-                    // if it can not be found in webpack output then check if it exists in the file system
-                    this.scriptName = findMatchingScriptName(this.options.scriptToRun, outputAssetNames);
-                    if (this.scriptName) {
-                        this.scriptPath = join(outputPath, this.scriptName);
-                    } else if (existsSync(this.options.scriptToRun)) {
-                        this.scriptName = parse(this.options.scriptToRun).base;
-                        this.scriptPath = normalize(this.options.scriptToRun);
-                    }
-
-                    if (!this.scriptPath) {
-                        Logger.error(
-                            LoggerMessages.NO_SCRIPT_NAME1 +
-                                this.options.scriptToRun +
-                                LoggerMessages.NO_SCRIPT_NAME2 +
-                                outputAssetNames +
-                                LoggerMessages.NO_SCRIPT_NAME3
-                        );
-                        return;
-                    }
-                } else {
-                    // if scriptToRun option is NOT set then try to guess which script should be run
-                    if (outputAssetNames.length === 1) {
-                        // if theres only 1 file in output assets choose it
-                        this.scriptName = outputAssetNames[0];
-                        this.scriptPath = join(outputPath, this.scriptName);
-                    } else {
-                        // otherwise try to find scripts named 'server.js' or 'index.js'
-                        this.scriptName =
-                            findMatchingScriptName('server.js', outputAssetNames) ||
-                            findMatchingScriptName('index.js', outputAssetNames);
-                        if (this.scriptName) {
-                            this.scriptPath = join(outputPath, this.scriptName);
-                        }
-                    }
-                }
-            }
-
-            if (!this.scriptPath) {
-                Logger.error(LoggerMessages.NO_SCRIPT_PATH1 + outputAssetNames + LoggerMessages.NO_SCRIPT_PATH2);
-                return;
-            }
-
-            if (this.scriptProcess && this.scriptProcess.connected) {
-                // if scriptProcess is running then kill it and start once again after it closes
-                Logger.info(LoggerMessages.RESTARTING + this.scriptName);
-                this.scriptProcess.on('close', () => (this.scriptProcess = fork(this.scriptPath, this.options.nodeArgs, this.options.processArgs)));
-                try {
-                    this.scriptProcess.kill('SIGKILL');
-                } catch (error) {
-                    console.error(error);
-                }
-            } else {
-                Logger.info(LoggerMessages.STARTING + this.scriptName);
-                try {
-                    this.scriptProcess = fork(this.scriptPath, this.options.nodeArgs, this.options.processArgs);
-                } catch (error) {
-                    console.error(error);
-                }
-            }
-        });
-    }
+      if (this.scriptProcess && this.scriptProcess.connected) {
+        // if scriptProcess is running then kill it and start once again after it closes
+        Logger.info(LoggerMessages.RESTARTING + this.scriptName);
+        this.scriptProcess.on(
+          'close',
+          () =>
+            (this.scriptProcess = fork(
+              this.scriptPath,
+              this.options.nodeArgs,
+              this.options.processArgs
+            ))
+        );
+        try {
+          this.scriptProcess.kill('SIGKILL');
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        Logger.info(LoggerMessages.STARTING + this.scriptName);
+        try {
+          this.scriptProcess = fork(
+            this.scriptPath,
+            this.options.nodeArgs,
+            this.options.processArgs
+          );
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    });
+  }
 }
 
-function findMatchingScriptName(scriptNameToFind: string, scriptNames: string[]) {
-    // check for literal matching
-    for (const name of scriptNames) {
-        if (name === scriptNameToFind) {
-            return name;
-        }
+function findMatchingScriptName(
+  scriptNameToFind: string,
+  scriptNames: string[]
+) {
+  // check for literal matching
+  for (const name of scriptNames) {
+    if (name === scriptNameToFind) {
+      return name;
     }
+  }
 
-    // if the above method fails check file names more liberally
-    for (const name of scriptNames) {
-        if (name.indexOf(scriptNameToFind) !== -1) {
-            return name;
-        }
+  // if the above method fails check file names more liberally
+  for (const name of scriptNames) {
+    if (name.indexOf(scriptNameToFind) !== -1) {
+      return name;
     }
+  }
 
-    return null;
+  return null;
 }
